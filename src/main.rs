9d2a8c60a7ad969330c0f358f5396aa6df36ef1c
Daniel Collin <daniel@collin.com>
2016-01-12 20:32:52 +0100

    Some initial code

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+#[macro_use]
+extern crate clap;
+
+fn main() {
+    let matches = clap_app!(open_depacker =>
+        (version: "0.1")
+        (author: "Daniel Collin. <daniel@collin.com>")
+        (about: "Generic data depacter")
+        (@arg input: +required "Sets the input file to use")
+        (@arg debug: -d ... "Sets the level of debugging information")
+    ).get_matches();
+
+    match matches.occurrences_of("debug") {
+        0 => println!("Debug mode is off"),
+        1 => println!("Debug mode is kind of on"),
+        2 => println!("Debug mode is on"),
+        3 | _ => println!("Don't be crazy"),
+    }
+
+    if let Some(ref file) = matches.value_of("input") {
+        println!("Using config file: {}", file);
+    }
+}
