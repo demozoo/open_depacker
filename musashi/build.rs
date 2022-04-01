ed48f4c1cab31b2440d769954e4b65b648c2a316
Daniel Collin <daniel@collin.com>
2022-04-01 08:38:21 +0200

    Upgraded Musashi depes

--- musashi/build.rs
+++ musashi/build.rs
@@ -1,17 +1,17 @@
-extern crate gcc;
-
 fn main() {
-    let mut cfg = gcc::Config::new();
+    let mut cfg = cc::Build::new();
 
     cfg.include("../external/musashi");
     cfg.include("../external/musashi/generated");
 
-    let files = &["../external/musashi/m68kcpu.c",
-                  "../external/musashi/m68kdasm.c",
-                  "../external/musashi/generated/m68kopac.c",
-                  "../external/musashi/generated/m68kopdm.c",
-                  "../external/musashi/generated/m68kopnz.c",
-                  "../external/musashi/generated/m68kops.c"];
+    let files = &[
+        "../external/musashi/m68kcpu.c",
+        "../external/musashi/m68kdasm.c",
+        "../external/musashi/generated/m68kopac.c",
+        "../external/musashi/generated/m68kopdm.c",
+        "../external/musashi/generated/m68kopnz.c",
+        "../external/musashi/generated/m68kops.c",
+    ];
 
     for f in files.iter() {
         cfg.file(*f);
